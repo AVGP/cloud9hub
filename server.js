93319403030822ce99e5e7c92ff22b84f431fe23
Martin Naumann <avgp_2k@yahoo.de>
2013-06-12 05:21:44 +0000

    Moved app.js to server.js

--- server.js
+++ server.js
@@ -11,16 +11,25 @@ var express = require('express')
 
 var app = express();
 
+var nextFreeWorkspacePort = 8080;
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.set('baseUrl', 'http://82.196.2.177');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(function(req, res, next) {
+    console.log(req.path);
+    if(/\/workspace\/.+/.test(req.path)) {
+        req.nextFreePort = (nextFreeWorkspacePort++);
+    }
+    next();
+});
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,12 +39,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//App
+
+// API
 app.get('/workspace', workspace.list);
-
-app.get('/workspace/create', workspace.create);
 app.post('/workspace/create', workspace.create);
-
 app.get('/workspace/:name', workspace.run);
 
 http.createServer(app).listen(app.get('port'), function(){
