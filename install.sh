946608102e967b4ff5474c68820407300a08660a
Martin N. <mr.avgp@googlemail.com>
2014-02-06 07:36:56 +0100

    Created a quick install script.

---
+++ install.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+
+echo "Installing Cloud9..."
+echo "-----------------------"
+
+git clone https://github.com/ajaxorg/cloud9.git c9
+cd c9
+npm install
+cd ..
+
+echo "Success."
+echo ""
+echo "Installing Cloud9Hub..."
+echo "-----------------------"
+
+git clone git@github.com:AVGP/cloud9hub.git
+cd cloud9hub
+npm install
+echo "Success."
+
+echo "Last steps"
+echo "-----------------------"
+echo "1. Create a Github app."
+echo "2. Copy cloud9hub/config.js.example to cloud9hub/config.js"
+echo "3. Edit your cloud9hub/config.js"
+echo ""
+echo "Have a lot of fun!"
