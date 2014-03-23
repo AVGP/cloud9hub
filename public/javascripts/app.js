690217220c0a97bf94c575498398f3599a198e2f
israelidanny <dannypovolotski@gmail.com>
2014-03-23 14:23:28 -0400

    angular-route is now an external module

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('c9hub', ['workspace']).config(function($routeProvider) {
+angular.module('c9hub', ['workspace', 'ngRoute']).config(function($routeProvider) {
     $routeProvider.when('/', {templateUrl: "/partials/login.html"});
     $routeProvider.when('/dashboard', {controller: WorkspaceCtrl, templateUrl: "/partials/workspace.html"});
 });
